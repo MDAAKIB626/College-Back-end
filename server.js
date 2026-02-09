a350576c9480e1778a4ff065e7673f1578b17bb3
MDAAKIB626 <mdaakibhussain382@gmail.com>
2026-02-10 00:17:27 +0530

    Deploy backend without DB

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app.js";
-import connectDB from "./config/db.js";
+// import connectDB from "./config/db.js";
+
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
-  await connectDB(); // ⭐ pehle DB
-  app.listen(process.env.PORT, () => {
-    console.log("🚀 Server running on port " + process.env.PORT);
+  // await connectDB(); // ❌ abhi OFF
+  app.listen(PORT, () => {
+    console.log("🚀 Server running without DB on port " + PORT);
   });
 };
 
